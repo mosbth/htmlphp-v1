0c121882f23b994fcbccb9f7b048073d1ccb3a14
Mikael Roos <mikael.t.h.roos@gmail.com>
2010-08-10 15:09:00 +0200

    with HTML5 validator link

---
+++ me.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Me-sidan</title>
+</head>
+
+<body>
+
+	<p>Här kommer snart min egen fina me-sida.</p>
+
+	<footer>
+		<a href='http://validator.w3.org/check/referer'>HTML5</a>
+	</footer>
+</body>
+	
+</html>
