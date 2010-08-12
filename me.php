4976d50003febf1f528a89f7ebbe4b1339f36b4c
Mikael Roos <mikael.t.h.roos@gmail.com>
2010-08-12 10:30:34 +0200

    Added link to W3C Unicorn

--- me.php
+++ me.php
@@ -15,7 +15,7 @@
 	<footer>
 		<a href="http://validator.w3.org/check/referer">HTML5</a> 
 		<a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a>
-		<a href="http://validator.w3.org/unicorn/check?ucn_uri=referer&ucn_task=conformance">Unicorn</a>
+		<a href="http://validator.w3.org/unicorn/check?ucn_uri=referer&amp;ucn_task=conformance">Unicorn</a>
 	</footer>
 </body>
 	
