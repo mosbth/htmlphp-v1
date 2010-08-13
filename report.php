ecb925bc4ba0a8fb85be725e49587c93479ddba7
Mikael Roos <mikael.t.h.roos@gmail.com>
2010-08-13 09:16:37 +0200

    Ändrade texten

--- report.php
+++ report.php
@@ -9,14 +9,15 @@
 			</header>
 			<p>
 				Här skriver du de redovisningstexter som anges i slutet av varje kursmoment.
-				Redovisningstexterna är oftast en reflektion över kursmomentet som besvarar ett antal frågor.
+				Redovisningstexterna är oftast en reflektion över kursmomentet. 
+				En reflektion som dessutom besvarar ett antal frågor.
 			</p>
 			<p>
 				Exempel på frågor kan vara följande. Vad gick bra? Var något svårt? Vad har du lärt dig?
 				Förbättringsförslag till kursmomentet?
 			</p>
 			<p>
-				Normalt skriver du ett textstycke om ca 15 meningar. Skriv så bra så att en utomstående, dvs dina 
+				Normalt skriver du ett textstycke om ca 15 meningar. Skriv så utförligt så att en utomstående, dvs dina 
 				lärare och med-studenter, förstår och kan sätta sig in i de problem och lösningar du jobbat igenom.
 			</p>
 		</section>
