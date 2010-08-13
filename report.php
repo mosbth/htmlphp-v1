d43d06e8b3ff22b4e7c7cf1c10c2b9f6edf314dc
Mikael Roos <mikael.t.h.roos@gmail.com>
2010-08-13 08:55:08 +0200

    Added page for reports

---
+++ report.php
@@ -0,0 +1,36 @@
+<?php include("header.php"); ?>
+
+	<!-- Here is the actual content of the page-->
+	<section>
+		<section>
+			<header>
+				<h1>Redovisning av htmlphp</h1>
+				<h2>Intro</h2>
+			</header>
+			<p>
+				Här skriver du de redovisningstexter som anges i slutet av varje kursmoment.
+				Redovisningstexterna är oftast en reflektion över kursmomentet som besvarar ett antal frågor.
+			</p>
+			<p>
+				Exempel på frågor kan vara följande. Vad gick bra? Var något svårt? Vad har du lärt dig?
+				Förbättringsförslag till kursmomentet?
+			</p>
+			<p>
+				Normalt skriver du ett textstycke om ca 15 meningar. Skriv så bra så att en utomstående, dvs dina 
+				lärare och med-studenter, förstår och kan sätta sig in i de problem och lösningar du jobbat igenom.
+			</p>
+		</header>
+	</section>
+		
+		<section>
+			<header>
+				<h2>Kmom01: Kom igång med HTML, CSS och PHP</h2>
+			</header>
+			<p>
+				Redovisningstext för kursmoment 01.
+			</p>
+		</section>
+	
+	</section>
+
+<?php include("footer.php"); ?>
