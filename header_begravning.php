7d7d46dfbff9e02c8f8be1c4cd4a8394cfcee3e5
Mikael Roos <mikael.t.h.roos@gmail.com>
2010-08-25 11:55:25 +0200

    Preparing to tag v3.0 with all project an delivery 3 included

--- header_begravning.php
+++ header_begravning.php
@@ -1,3 +1,3 @@
 		<nav id="begravning">
-			<a id="begr-seder-" href="begravningsseder_och_btuk-php.php">Seder&amp;bruk</a> 
+			<a id="begr-seder-" href="begravningsseder_och_bruk.php">Seder&amp;bruk</a> 
 		</nav>
