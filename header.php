96963fa3c4a28f549126002d3178a95dd2a51eb8
Mikael Roos <mikael.t.h.roos@gmail.com>
2010-08-25 15:48:42 +0200

    Included a style chooser and retagging v3.0

--- header.php
+++ header.php
@@ -1,4 +1,9 @@
-<?php error_reporting(-1); ?>
+<?php 
+$sessionName = 'htmlphp_mos';
+session_name($sessionName); 
+session_start(); 
+error_reporting(-1); 
+?>
 
 <!DOCTYPE html>
 <html lang="sv">
@@ -7,19 +12,21 @@
 	<meta charset="utf-8">
 
 	<!-- Use meta to ease indexing made by search engines -->
-	<meta name="keywords" content="HTML, CSS, PHP, SQL">
+	<meta name="keywords"    content="HTML, CSS, PHP, SQL">
  	<meta name="description" content="Distansundervisning av Databaser och Webb">
- 	<meta name="author" content="Mikael Roos, mos@bth.se">	
- 	<meta name="copyright" content="Copyright 2010">	
+ 	<meta name="author"      content="Mikael Roos, mos@bth.se">	
+ 	<meta name="copyright"   content="Copyright 2010">	
  		
 	<!-- Stylesheets -->
-	<link rel="stylesheet" media="all" href="style/stylesheet.css" title="Mos standard grey" type="text/css">
-	<link rel="stylesheet" media="print" href="style/print.css" type="text/css">
-	<link rel="alternative stylesheet" href="style/stylesheet_red.css" title="Mos standard red" type="text/css">
-	<link rel="alternative stylesheet" href="style/stylesheet_bth.css" title="Mos BTH-ish" type="text/css">
-	<link rel="alternative stylesheet" href="style/stylesheet_print.css" title="Mos to print" type="text/css">
+	<?php include("choose_style.php"); ?>
+	<link rel="stylesheet" media="all"   type="text/css" href="<?php echo $stylePath; ?>"  title="<?php echo $styleTitle; ?>">
+	<link rel="stylesheet" media="print" type="text/css" href="style/print.css">
+	<link rel="alternative stylesheet"   type="text/css" href="style/stylesheet.css"       title="Mos standard grey">
+	<link rel="alternative stylesheet"   type="text/css" href="style/stylesheet_red.css"   title="Mos standard red">
+	<link rel="alternative stylesheet"   type="text/css" href="style/stylesheet_bth.css"   title="Mos BTH-ish">
+	<link rel="alternative stylesheet"   type="text/css" href="style/stylesheet_print.css" title="Mos to print">
 
-	<!-- The small icon displayed by the browser -->
+	<!-- The small icon displayed by the browser next to the link -->
 	<link rel="shortcut icon" href="img/favicon.ico">
 
 	<!-- Use PHP to set the page title dynamic -->
@@ -34,13 +41,17 @@
 
 	<!-- Top header with logo and navigation -->
 	<header id="top">
+
+		<!-- Use PHP to print the form to choose style -->
+		<?php echo $formChooseStyle; ?>
+
 		<img src="img/logo.png" alt="htmlphp me" width=200 height=100>
 		<nav>
-			<a id="me-" href="me.php">Me</a> 
+			<a id="me-"     href="me.php">Me</a> 
 			<a id="report-" href="report.php">Redovisning</a> 
 			<a id="html20-" href="de_20_vanligaste_html_elementen.php">HTML-20</a> 
-			<a id="css20-" href="de_20_vanligaste_css_konstruktionerna.php">CSS-20</a> 
-			<a id="proj-" href="projekt.php">Projekt</a> 
-			<a id="begr-" href="begravningsseder_och_bruk.php">Begravningsmuseum</a> 
+			<a id="css20-"  href="de_20_vanligaste_css_konstruktionerna.php">CSS-20</a> 
+			<a id="proj-"   href="projekt.php">Projekt</a> 
+			<a id="begr-"   href="begravningsseder_och_bruk.php">Begravningsmuseum</a> 
 		</nav>
 	</header>
