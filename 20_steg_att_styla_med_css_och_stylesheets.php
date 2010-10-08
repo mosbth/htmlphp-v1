111f56d792b90f3dc95373ebd6ecd909f2eac4f8
Mikael Roos <mikael.t.h.roos@gmail.com>
2010-10-08 17:08:22 +0200

    Restructuring meny and adding articles

---
+++ 20_steg_att_styla_med_css_och_stylesheets.php
@@ -0,0 +1,271 @@
+<?php
+$title = "20 steg att styla med CSS och stylesheets";
+$bodyId = "guide-style20";
+include("header.php");
+include("header_guider.php");
+?>
+
+<!-- - - - - - - - - - - - - - - - - - wrap whole content - - - - - - - - - - - - - - -->
+<div id="wrap">
+
+<!-- - - - - - - - - - - - - - - - - - aside        - - - - - - - - - - - - - - - - - -->
+<aside class="right">
+</aside>
+
+<!-- - - - - - - - - - - - - - - - - - article      - - - - - - - - - - - - - - - - - -->
+<article>
+	<header id="start">
+		<h1>20 steg att styla med CSS och stylesheets</h1>
+
+			<p class="byline">Av Mikael Roos, mos@bth.se, <time pubdate datetime="2010-10-10">1:e 
+			oktober 2010</time>
+
+			<p class="ingress">En introduktion på 20 steg för att styla din webbplats med CSS och stylesheets.
+
+			<p>Bästa sättet att genomföra övningen är att du själv går igenom och studerar alla exemplen. 
+			parallellt med det gör du din egna stylesheet och väljer vad du vill implementera från de olika exemplen.
+			Läsa är bra men man måste göra själv, "kan själv", för att lära sig.
+			
+			<ol>
+				<li><a href="#s1">Style! på dbwebb.se/style</a>
+				<li><a href="#s2">Struktur och modulärt tänkande med stylesheets</a>
+				<li><a href="#s3"></a>
+				<li><a href="#s4"></a>
+				<li><a href="#s5"></a>
+				<li><a href="#s6"></a>
+				<li><a href="#s7"></a>
+				<li><a href="#s8"></a>
+				<li><a href="#s9"></a>
+				<li><a href="#s10"></a>
+				<li><a href="#s11"></a>
+				<li><a href="#s12"></a>
+				<li><a href="#s13"></a>
+				<li><a href="#s14"></a>
+				<li><a href="#s15"></a>
+				<li><a href="#s16"></a>
+				<li><a href="#s17"></a>
+				<li><a href="#s18"></a>
+				<li><a href="#s19"></a>
+				<li><a href="#s20">Andas</a>
+			</ol>
+	
+	</header>
+
+<!-- - - - - - - - - - - - - - - - - - section       - - - - - - - - - - - - - - - - - -->
+	<section id="s1">
+		<h2>1. Style! på dbwebb.se/style</h2>			
+		<p>Förklara verktyget hur det funkar. Även css och html export.
+		
+		<p>Egna exempel välkomna, maila mos eller rickard.
+		
+		<p><a href="http://dbwebb.se/style/?id=1">Exempel</a>
+
+		<p class="go-to-start"><a href="#start">Gå till toppen av artikeln</a></p>
+	</section>
+
+<!-- - - - - - - - - - - - - - - - - - section       - - - - - - - - - - - - - - - - - -->
+	<section id="s2">
+		<h2>2. Struktur och modulärt tänkande med stylesheets</h2>
+		
+		<p>Visa @import och hur man kan dela upp en stylesheet i moduler. Fördelar/nackdelar.
+		
+		<p><a href="http://dbwebb.se/style/?id=1">Exempel</a>
+		
+		<p class="go-to-start"><a href="#start">Gå till toppen av artikeln</a></p>
+	</section>
+
+<!-- - - - - - - - - - - - - - - - - - section       - - - - - - - - - - - - - - - - - -->
+	<section id="s3">
+		<h2>3. </h2>
+		
+		<p>Text.
+		
+		<p><a href="http://dbwebb.se/style/?id=1">Exempel</a>
+				
+		<p class="go-to-start"><a href="#start">Gå till toppen av artikeln</a></p>
+	</section>
+
+<!-- - - - - - - - - - - - - - - - - - section       - - - - - - - - - - - - - - - - - -->
+	<section id="s4">
+		<h2>4. </h2>
+		
+		<p>Text.
+		
+		<p><a href="http://dbwebb.se/style/?id=1">Exempel</a>		
+		
+		<p class="go-to-start"><a href="#start">Gå till toppen av artikeln</a></p>
+	</section>
+
+<!-- - - - - - - - - - - - - - - - - - section       - - - - - - - - - - - - - - - - - -->
+	<section id="s5">
+		<h2>5. </h2>
+		
+		<p>Text.
+		
+		<p><a href="http://dbwebb.se/style/?id=1">Exempel</a>		
+		
+		<p class="go-to-start"><a href="#start">Gå till toppen av artikeln</a></p>
+	</section>
+
+<!-- - - - - - - - - - - - - - - - - - section       - - - - - - - - - - - - - - - - - -->
+	<section id="s6">
+		<h2>6. </h2>
+		
+		<p>Text.
+		
+		<p><a href="http://dbwebb.se/style/?id=1">Exempel</a>
+		
+		<p class="go-to-start"><a href="#start">Gå till toppen av artikeln</a></p>
+	</section>
+
+<!-- - - - - - - - - - - - - - - - - - section       - - - - - - - - - - - - - - - - - -->
+	<section id="s7">
+		<h2>7. </h2>
+		
+		<p>Text.
+		
+		<p><a href="http://dbwebb.se/style/?id=1">Exempel</a>
+		
+		<p class="go-to-start"><a href="#start">Gå till toppen av artikeln</a></p>
+
+<!-- - - - - - - - - - - - - - - - - - section       - - - - - - - - - - - - - - - - - -->
+	<section id="s8">
+		<h2>8. </h2>
+		
+		<p>Text.
+		
+		<p><a href="http://dbwebb.se/style/?id=1">Exempel</a>
+		
+		<p class="go-to-start"><a href="#start">Gå till toppen av artikeln</a></p>
+
+<!-- - - - - - - - - - - - - - - - - - section       - - - - - - - - - - - - - - - - - -->
+	<section id="s9">
+		<h2>9. </h2>
+		
+		<p>Text.
+		
+		<p><a href="http://dbwebb.se/style/?id=1">Exempel</a>
+		
+		<p class="go-to-start"><a href="#start">Gå till toppen av artikeln</a></p>
+
+<!-- - - - - - - - - - - - - - - - - - section       - - - - - - - - - - - - - - - - - -->
+	<section id="s10">
+		<h2>10.</h2>
+		
+		<p>Text.
+		
+		<p><a href="http://dbwebb.se/style/?id=1">Exempel</a>
+		
+
+		<p class="go-to-start"><a href="#start">Gå till toppen av artikeln</a></p>
+
+<!-- - - - - - - - - - - - - - - - - - section       - - - - - - - - - - - - - - - - - -->
+	<section id="s11">
+		<h2>11. </h2>
+		
+		<p>Text.
+		
+		<p><a href="http://dbwebb.se/style/?id=1">Exempel</a>
+		
+
+		<p class="go-to-start"><a href="#start">Gå till toppen av artikeln</a></p>
+
+<!-- - - - - - - - - - - - - - - - - - section       - - - - - - - - - - - - - - - - - -->
+	<section id="s12">
+		<h2>12.</h2>
+
+		<p>Text.
+		
+		<p><a href="http://dbwebb.se/style/?id=1">Exempel</a>
+		
+
+		<p class="go-to-start"><a href="#start">Gå till toppen av artikeln</a></p>
+
+<!-- - - - - - - - - - - - - - - - - - section       - - - - - - - - - - - - - - - - - -->
+	<section id="s13">
+		<h2>13. </h2>
+		
+		<p>Text.
+		
+		<p><a href="http://dbwebb.se/style/?id=1">Exempel</a>
+		
+
+		<p class="go-to-start"><a href="#start">Gå till toppen av artikeln</a></p>
+
+<!-- - - - - - - - - - - - - - - - - - section       - - - - - - - - - - - - - - - - - -->
+	<section id="s14">
+		<h2>14. </h2>
+		
+		<p>Text.
+		
+		<p><a href="http://dbwebb.se/style/?id=1">Exempel</a>
+		
+		<p class="go-to-start"><a href="#start">Gå till toppen av artikeln</a></p>
+
+<!-- - - - - - - - - - - - - - - - - - section       - - - - - - - - - - - - - - - - - -->
+	<section id="s15">
+		<h2>15. </h2>
+		
+		<p>Text.
+		
+		<p><a href="http://dbwebb.se/style/?id=1">Exempel</a>
+		
+		<p class="go-to-start"><a href="#start">Gå till toppen av artikeln</a></p>
+
+<!-- - - - - - - - - - - - - - - - - - section       - - - - - - - - - - - - - - - - - -->
+	<section id="s16">
+		<h2>16. </h2>
+		
+		<p>Text.
+		
+		<p><a href="http://dbwebb.se/style/?id=1">Exempel</a>
+		
+		<p class="go-to-start"><a href="#start">Gå till toppen av artikeln</a></p>
+
+<!-- - - - - - - - - - - - - - - - - - section       - - - - - - - - - - - - - - - - - -->
+	<section id="s17">
+		<h2>17. </h2>
+		
+		<p>Text.
+		
+		<p><a href="http://dbwebb.se/style/?id=1">Exempel</a>
+		
+		<p class="go-to-start"><a href="#start">Gå till toppen av artikeln</a></p>
+
+<!-- - - - - - - - - - - - - - - - - - section       - - - - - - - - - - - - - - - - - -->
+	<section id="s18">
+		<h2>18. </h2>
+		
+		<p>Text.
+		
+		<p><a href="http://dbwebb.se/style/?id=1">Exempel</a>
+		
+		<p class="go-to-start"><a href="#start">Gå till toppen av artikeln</a></p>
+
+<!-- - - - - - - - - - - - - - - - - - section       - - - - - - - - - - - - - - - - - -->
+	<section id="s19">
+		<h2>19. </h2>
+		
+		<p>Text.
+		
+		<p><a href="http://dbwebb.se/style/?id=1">Exempel</a>
+		
+		<p class="go-to-start"><a href="#start">Gå till toppen av artikeln</a></p>
+
+<!-- - - - - - - - - - - - - - - - - - section       - - - - - - - - - - - - - - - - - -->
+	<section id="s20">
+		<h2>20. Andas</h2>
+		
+		<p>Bra jobbat!
+
+		<p class="go-to-start"><a href="#start">Gå till toppen av artikeln</a></p>
+
+
+<!-- - - - - - - - - - - - - - - - - - footer         - - - - - - - - - - - - - - - - - -->
+	<?php include("byline.php"); ?>
+	
+</article>
+
+</div> <!-- id=wrap -->
+
+<?php include("footer.php"); ?>
